09711e8b73ddd2a2b4a113fa343ee7d80aa93ae2
thrilokyapriya2002 <thrilokyapriya2002@gmail.com>
2024-07-02 13:10:00 +0530

    feature file

---
+++ cppfile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; // You need to specify the namespace std
+
+int a = 90;
+int b = 100;
+
+int c = a + b;
+
+int main() { // Code that outputs to console should be inside a function, typically main()
+
+    cout << c << endl;
+
+    return 0;
+}
